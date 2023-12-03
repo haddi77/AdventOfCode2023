5403f3df2ee2c6004781ae4c20dd68414eb80179
Hafsteinn Már Heimisson <haddi77@gmail.com>
2023-12-03 19:38:05 +0000

    day1 part 2 and day 2 part 1

--- day1/main.py
+++ day1/main.py
@@ -10,27 +10,57 @@ ALPHANUMBERS = {
     "nine": 9
 }
 
-lines = open('sample.txt', 'r', encoding='utf8').readlines()
+lines = open('input.txt', 'r', encoding='utf8').readlines()
 
 sum = 0
 
 
 
 for line in lines:
-    line = line.lower()
-    for key, value in ALPHANUMBERS.items():
-        line = line.replace(key, key + str(value))
+    line = line.lower().strip()
+    # for key, value in ALPHANUMBERS.items():
+    #     line = line.replace(key, key + str(value))
 
     calibration_value = ''
-    for letter in line:
+    for idx, letter in enumerate(line):
         if letter.isdigit():
             calibration_value += letter
             break
 
-    for letter in reversed(line):
+        for key, value in ALPHANUMBERS.items():
+            try:
+                found_alphanumber = line.index(key,idx)
+
+            except ValueError:
+                continue
+
+            if found_alphanumber == idx:
+                calibration_value += str(value)
+                break
+
+        if len(calibration_value) > 0:
+            break
+
+
+    for idx, letter in enumerate(reversed(line)):
         if letter.isdigit():
             calibration_value += letter
             break
+
+        for key, value in ALPHANUMBERS.items():
+            try:
+                found_alphanumber = line.index(key,len(line)-1-idx)
+
+            except ValueError:
+                continue
+
+            if found_alphanumber == len(line)-1-idx:
+                calibration_value += str(value)
+                break
+
+        if len(calibration_value) > 1:
+            break
+
     print(calibration_value)
     sum += int(calibration_value)
 
---
+++ day2/main.py
@@ -0,0 +1,40 @@
+
+lines = open('sample.txt', 'r', encoding='utf8')
+
+bag = {
+    'red': 12,
+    'green': 13,
+    'blue': 14
+}
+
+sum = 0
+
+while lines:
+    line = lines.readline().strip()
+    if line == '':
+        break
+
+    game_id = int(line.split(': ')[0].replace('Game ', ''))
+    rounds_text = line.split(': ')[1].split('; ')
+
+    for round_text in rounds_text:
+        for color in round_text.split(', '):
+            for bag_color_name, bag_color_amount in bag.items():
+                if bag_color_name in color:
+                    color_amount = int(color.replace(bag_color_name, '').strip())
+                    if color_amount > bag_color_amount:
+                        break
+            else:
+                continue
+
+            break
+
+        else:
+            continue
+
+        break
+
+    else:
+        sum += game_id
+
+print(sum)
