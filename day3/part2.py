9caea85d1354682359552908871996f656b82232
Hafsteinn Már Heimisson <haddi77@gmail.com>
2023-12-04 00:03:34 +0000

    day3 part 2

--- day3/part2.py
+++ day3/part2.py
@@ -1,7 +1,6 @@
 from string import punctuation
 
-symbols = set(punctuation)
-symbols.remove('.')
+gear = '*'
 
 lines = open('input.txt', 'r', encoding='utf8')
 
@@ -10,6 +9,9 @@ part_no_sum = 0
 line_prev = ''
 line_cur = lines.readline().strip()
 line_next = lines.readline().strip()
+line_no = 0
+
+gears = {}
 
 while lines:
     num = None
@@ -39,26 +41,45 @@ while lines:
 
             if line_prev:
                 neighbours_top = line_prev[other_lines_start_at: other_lines_end_at + 1]
+                if gear in neighbours_top:
+                    gear_address = f'{line_no - 1},{neighbours_top.index(gear) + other_lines_start_at}'
+                    if gear_address not in gears:
+                        gears[gear_address] = [int(num)]
+
+                    else:
+                        gears[gear_address] += [int(num)]
 
             if line_next:
                 neighbours_bottom = line_next[other_lines_start_at: other_lines_end_at + 1]
+                if gear in neighbours_bottom:
+                    # new_gears = {f'{line_no + 1},{neighbours_bottom.index(gear) + other_lines_start_at}': num}
+                    # gears.update(new_gears)
+                    gear_address = f'{line_no + 1},{neighbours_bottom.index(gear) + other_lines_start_at}'
+                    if gear_address not in gears:
+                        gears[gear_address] = [int(num)]
+
+                    else:
+                        gears[gear_address] += [int(num)]
 
             if num_start_idx > 0:
                 neighbour_left = line_cur[num_start_idx - 1]
+                if neighbour_left == gear:
+                    gear_address = f'{line_no},{num_start_idx - 1}'
+                    if gear_address not in gears:
+                        gears[gear_address] = [int(num)]
+
+                    else:
+                        gears[gear_address] += [int(num)]
 
             if num_end_idx < len(line_cur) - 1:
                 neighbour_right = line_cur[num_end_idx + 1]
+                if neighbour_right == gear:
+                    gear_address = f'{line_no},{num_end_idx + 1}'
+                    if gear_address not in gears:
+                        gears[gear_address] = [int(num)]
 
-
-            neighbours = set(neighbour_left + neighbour_right + neighbours_top + neighbours_bottom)
-
-            nearby_parts = neighbours.intersection(symbols)
-            if len(nearby_parts) > 0:
-                print(f'{num} is the part number for {nearby_parts}')
-                part_no_sum += int(num)
-
-            else:
-                print(f'{num} is not a part number')
+                    else:
+                        gears[gear_address] += [int(num)]
 
             num = None
             num_start_idx = None
@@ -69,5 +90,10 @@ while lines:
         break
 
     line_next = lines.readline().strip()
+    line_no += 1
 
-print(f'The sum of all part numbers is {part_no_sum}')
+for gear, gear_nos in gears.items():
+    if len(gear_nos) > 1:
+        part_no_sum += gear_nos[0] * gear_nos[1]
+
+print(part_no_sum)
